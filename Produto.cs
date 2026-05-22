f5e7676e2e5d353041459874bae26468299705be
Gabriel Alves <gabriel.f.alves@radixeng.com>
2026-05-21 22:33:48 -0300

    Second commit

---
+++ Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace raizes_do_nordeste
+{
+    public class Produto
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public float Valor { get; set; }
+    }
+}
